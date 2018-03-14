84e91cb14bc8d72b932b652ab1ae728c28fe3a05
Qamar Stationwala <yopappy0@gmail.com>
2018-03-13 19:52:02 -0700

    upload page functional ...?

--- client/src/pages/Upload.js
+++ client/src/pages/Upload.js
@@ -1,37 +1,59 @@
 import React from 'react';
 import Genre from '../components/Genre.js';
-
+import axios from 'axios'
 class Upload extends React.Component {
+    onChangeHandler = (event) => {
+        this.setState({
+            [event.target.name]: event.target.value
+        }, () => {
+            console.log(this.state);
+        })
+    }
+    submitBook = (event) => {
+        event.preventDefault();
+        const loginToken = window.localStorage.getItem("token");
+        let data = new FormData();
+        if (!this.state.genre1) {
+            return false;
+        }
+        data.append("title", this.state.title) 
+        let genre = this.state.genre1;
+        if (this.state.genre2) genre += "," + this.state.genre2;
+        if (this.state.genre3) genre += "," + this.state.genre3;
+        data.append("genre", genre);
+        data.append("description", this.state.description);
+        data.append("userId", JSON.parse(window.atob(loginToken.split('.')[1])).id);
+        data.append("price", this.state.bookPrice);
+        data.append("bookFile", document.getElementById("pdf-file").files[0])
+        axios.post('/api/books/upload', data, { headers: { "Authorization": "Bearer " + loginToken } })
+        .then((data) => {
+            console.log(data)
+        }).catch((error) => {
+            console.error(error);
+        })
+    }
     render() {
         return(
             <div id="main">
                 <h5>Upload your stories here</h5>
-                <form enctype="multipart/form-data" action="/upload/image" method="post">
-                    <label>
-                        Title:
-                        <input id="upload-title" type="text" />
-                    </label><br />
-                    <label>
-                        Genre:
-                        <div class="genre-box">
-                            <Genre />
-                        </div>
-                        <div class="genre-box">
-                            <Genre />
-                        </div>
-                        <div class="genre-box">
-                            <Genre />
-                        </div>
-                    </label>
+                <form encType="multipart/form-data">
+                    <label htmlFor="title">Title:</label>
+                    <input onChange={this.onChangeHandler} name="title" id="upload-title" type="text" />
                     <br />
-                    <label>
-                        Synopsis:
-                        <input id="upload-synopsis" type="text" />
-                    </label>
-                    <label>
-                        <input id="image-file" type="file" />
-                    </label><br />
-                    <input class="submit" type="submit" value="Submit" />
+                    <Genre name="genre1" onChangeHandler={this.onChangeHandler} />
+                    <Genre name="genre2" onChangeHandler={this.onChangeHandler} />
+                    <Genre name="genre3" onChangeHandler={this.onChangeHandler} />
+                    <br />
+                    <label htmlFor="description">Synopsis:</label>
+                    <input onChange={this.onChangeHandler} name="description" id="upload-synopsis" type="text" />
+                    <br />
+                    <label htmlFor="bookPrice">Price: </label>
+                    <input onChange={this.onChangeHandler} name="bookPrice" type="number" min="0" step="0.01" />
+                    <br />
+                    <label htmlFor="bookFile">Select PDF:</label>
+                    <input name="bookFile" onChange={this.onChangeHandler} id="pdf-file" type="file" />
+                    <br />
+                    <input onClick={this.submitBook} class="submit" type="submit" value="Submit" />
                 </form>
             </div>
         );
