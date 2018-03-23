c2839f396c9eb37884e9eafa41013c8a106505aa
jpmalo <malomedia7@gmail.com>
2018-03-22 21:10:55 -0700

    Various styling and front end changes

--- client/src/App.js
+++ client/src/App.js
@@ -19,10 +19,10 @@ class App extends Component {
         <div>
         <Particles canvasClassName="particles" 
           params={
-            {"particles":{"number":{"value":6,"density":{"enable":true,"value_area":800}},
+            {"particles":{"number":{"value":6,"density":{"enable":false,"value_area":800}},
             "color":{"value":"#1b1e34"},"shape":{"type":"polygon","stroke":{"width":0,"color":"#000"},"polygon":{"nb_sides":6},
             "image":{"src":"img/github.svg","width":100,"height":100}},"opacity":{"value":0.07,"random":true,
-            "anim":{"enable":false,"speed":1,"opacity_min":0.1,"sync":false}},"size":{"value":160,"random":false,"anim":{"enable":true,"speed":10,"size_min":40,"sync":false}},
+            "anim":{"enable":false,"speed":1,"opacity_min":0.0,"sync":false}},"size":{"value":160,"random":false,"anim":{"enable":true,"speed":10,"size_min":40,"sync":false}},
             "line_linked":{"enable":false,"distance":200,"color":"#ffffff","opacity":1,"width":2},"move":{"enable":true,"speed":8,"direction":"none","random":false,
             "straight":false,"out_mode":"out","bounce":false,"attract":{"enable":false,"rotateX":600,"rotateY":1200}}},
             "interactivity":{"detect_on":"canvas","events":{"onhover":{"enable":false,"mode":"grab"},"onclick":{"enable":false,"mode":"push"},
