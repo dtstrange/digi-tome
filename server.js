47237135e71b2f69f931cec1ed90ea3dd769db43
Dustin Frank <dustintstrange@gmail.com>
2018-03-24 09:42:15 -0700

    removed /build from .gitignore

--- server.js
+++ server.js
@@ -37,10 +37,10 @@ if (process.env.NODE_ENV === 'production') {
 //     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
 //     next();
 //   });
-app.get("*", (req, res)=>{
-    // res.sendFile("./client/public/index.html")
-    res.send("Hello");
-})
+// app.get("*", (req, res)=>{
+//     // res.sendFile("./client/public/index.html")
+    
+// })
 app.use("/api/user", authRoutes);
 app.use(express.static("books"))
 app.use(jwt({
