a0a72b75ff1759f6b54d5bd5bd36259dd87aaef1
Dustin Frank <dustintstrange@gmail.com>
2018-03-24 10:43:52 -0700

    heroku test

--- server.js
+++ server.js
@@ -27,7 +27,7 @@ app.use(bodyParser.text());
 app.use(bodyParser.json({ type: "application/vnd.api+json" }));
 
 // if (process.env.NODE_ENV === 'production') {
-//     app.use(express.static('client/build'));
+    app.use(express.static('client/build'));
 // }
 // app.use(express.static(path.join(__dirname, 'public')));
 
