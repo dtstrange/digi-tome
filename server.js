6035158bd12470a42a595e32af65ef1c73b016b6
Dustin Frank <dustintstrange@gmail.com>
2018-03-24 10:39:21 -0700

    heroku test

--- server.js
+++ server.js
@@ -29,9 +29,7 @@ app.use(bodyParser.json({ type: "application/vnd.api+json" }));
 if (process.env.NODE_ENV === 'production') {
     app.use(express.static('client/build'));
 }
-//app.use(express.static("books"))
-
-// app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, 'public')));
 
 
 //routes
@@ -45,10 +43,11 @@ if (process.env.NODE_ENV === 'production') {
     
 // })
 app.use("/api/user", authRoutes);
-app.use(jwt({
-    secret: process.env.JWT_SECRET,
-    userProperty: 'payload'
-}));
+app.use(express.static("books"))
+// app.use(jwt({
+//     secret: process.env.JWT_SECRET,
+//     userProperty: 'payload'
+// }));
 app.use("/api/books", bookRoutes);
 app.use("/api/profile", profileRoute);
 
