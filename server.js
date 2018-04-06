3b115796a784b4673bd6cc39264c38beacd63bca
Qamar Stationwala <yopappy0@gmail.com>
2018-04-05 17:00:46 -0700

    uncommented express static client build

--- server.js
+++ server.js
@@ -27,7 +27,7 @@ app.use(bodyParser.text());
 app.use(bodyParser.json({ type: "application/vnd.api+json" }));
 
 if (process.env.NODE_ENV === 'production') {
-//    app.use(express.static('client/build'));
+   app.use(express.static('client/build'));
    app.get("/", (req, res) => {
     res.sendFile(path.join(__dirname, '/client/build/index.html'));     
    })
